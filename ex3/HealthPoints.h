c31560f9ae1fe5dc795f4cc8f4b8be64a5bbec73
Sleeman nabwani <103364590+sleeman-nabwani@users.noreply.github.com>
2022-05-24 20:31:34 +0300

    Update HealthPoints.h

--- ex3/HealthPoints.h
+++ ex3/HealthPoints.h
@@ -1,9 +1,13 @@
-#include<iostream>
+#include <iostream>
+#define MAX_HP 100
+#include <string>
+#include <ostream>
+using std::ostream;
 
 class HealthPoints
 {
 public :
-	HealthPoints(int maxHp=100);
+	HealthPoints(int maxHp=MAX_HP);
 	~HealthPoints() = default;
 	HealthPoints(const HealthPoints&) = default;
 	HealthPoints& operator=(const HealthPoints&) = default;
@@ -11,13 +15,19 @@ public :
 	HealthPoints& operator-=(const int&);
 	HealthPoints& operator-(const int&)const;
 	HealthPoints& operator+(const int&) const;
+	bool operator==(const HealthPoints& hp)const;
+        bool operator !=(const HealthPoints& hp)const;
+        bool operator >(const HealthPoints& healthPoints)const;
+        bool operator >=(const HealthPoints& healthPoints)const;
+        bool operator <(const HealthPoints& hp)const;
+        bool operator <=(const HealthPoints& hp)const;
+	
+        friend ostream& operator <<(ostream& os,const HealthPoints& hp);
+
 	class InvalidArgument {};
 private:
 	int m_maxHp;
-	int m_Hp;
-};
-class HealthPoints::InvalidArgument
+	@@ -36,5 +20,4 @@ class HealthPoints::InvalidArgument
 {
 public :
 	InvalidArgument() {}
-};
