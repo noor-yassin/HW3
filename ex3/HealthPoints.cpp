1f0dbc446d1036246f52dc82b7096c15712f4d5a
Sleeman nabwani <103364590+sleeman-nabwani@users.noreply.github.com>
2022-05-24 18:22:25 +0300

    Add files via upload

--- ex3/HealthPoints.cpp
+++ ex3/HealthPoints.cpp
@@ -1,1 +1,46 @@
-#pragma once
+#include<iostream>
+#include "HealthPoints.h"
+
+HealthPoints::HealthPoints(int maxHp)
+{
+	m_maxHp = maxHp;
+	m_Hp = m_maxHp;
+}
+HealthPoints& HealthPoints::operator+=(const int& health)
+{
+	m_Hp += health;
+	if (m_Hp > m_maxHp)
+	{
+		m_Hp = m_maxHp;
+	}
+	if (m_Hp < 0)
+	{
+		m_Hp = 0;
+	}
+	return *this;
+}
+HealthPoints& HealthPoints::operator-=(const int& health)
+{
+	if (health <= 0)
+		throw InvalidArgument();
+	m_Hp -= health;
+	if (m_Hp > m_maxHp)
+	{
+		m_Hp = m_maxHp;
+	}
+	if (m_Hp < 0)
+	{
+		m_Hp = 0;
+	}
+	return *this;
+}
+HealthPoints& HealthPoints::operator+(const int& health)const
+{
+	HealthPoints result =*this;
+	return(result += health);
+}
+HealthPoints& HealthPoints::operator-(const int& health)const
+{
+	HealthPoints result = *this;
+	return(result -= health);
+}
--- ex3/HealthPoints.h
+++ ex3/HealthPoints.h
@@ -1,1 +1,22 @@
-#pragma once
+
+class HealthPoints
+{
+public :
+	HealthPoints(int maxHp=100);
+	~HealthPoints() = default;
+	HealthPoints(const HealthPoints&) = default;
+	HealthPoints& operator=(const HealthPoints&) = default;
+	HealthPoints& operator+=(const int&);
+	HealthPoints& operator-=(const int&);
+	HealthPoints& operator-(const int&)const;
+	HealthPoints& operator+(const int&) const;
+	class InvalidArgument {};
+private:
+	int m_maxHp;
+	int m_Hp;
+};
+class HealthPoints::InvalidArgument
+{
+public :
+	InvalidArgument() {}
+};
