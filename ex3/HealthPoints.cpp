cb8968c6a11e6932f69c8f2f2256b902aafcff13
Sleeman nabwani <103364590+sleeman-nabwani@users.noreply.github.com>
2022-05-25 13:07:32 +0300

    Add files via upload

--- ex3/HealthPoints.cpp
+++ ex3/HealthPoints.cpp
@@ -44,3 +44,54 @@ HealthPoints& HealthPoints::operator-(const int& health)const
 	HealthPoints result = *this;
 	return(result -= health);
 }
+bool HealthPoints::operator==(const HealthPoints& hp)const
+{
+	return m_Hp == hp.m_Hp;
+}
+
+bool HealthPoints::operator !=(const HealthPoints& hp)const
+{
+	if (m_Hp != hp.m_Hp)
+	{
+		return true;
+	}
+	return false;
+}
+
+bool HealthPoints::operator >(const HealthPoints& healthPoints)const
+{
+	return(m_Hp > healthPoints.m_Hp);
+}
+
+bool HealthPoints::operator>=(const HealthPoints& healthPoints) const
+{
+	return (m_Hp>=healthPoints.m_Hp);
+}
+
+bool HealthPoints::operator<(const HealthPoints& hp)const
+{
+	return(m_Hp < hp.m_Hp);
+}
+
+bool HealthPoints::operator<=(const HealthPoints& hp)const
+{
+	if (m_Hp<=hp.m_Hp)
+	{
+		return true;
+	}
+	return false;
+}
+
+bool operator==(const int& num, const HealthPoints& health)
+{
+	if (health.m_Hp == num)
+		return true;
+	return false;
+}
+
+ostream& operator <<(ostream& os, const HealthPoints& hp)
+{
+	return os << hp.m_Hp << "(" << hp.m_maxHp << ")" << std::endl;
+}
+
+
